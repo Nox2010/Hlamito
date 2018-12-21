7bb1281f9d09f52b9cade70a2cf80a98c3dd6bd2
Nox2010 <Nox2010@gmail.com>
2018-12-22 01:29:30 +0500

    Пробую бутстрап

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'welcome/index'
+  get 'articles/index'
 
   resources :articles do
     resources :comments
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'welcome#index'
+  root 'articles#index'
 end
